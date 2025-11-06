c65407ec1ea2873286563efdfcb5d05ade21ddb4
Ji Minwoo <jimw0201@gmail.com>
2025-11-07 01:36:17 +0900

    shader 분리
    shader.cpp / shader.h

--- Game/CG_project/parking_master.cpp
+++ Game/CG_project/parking_master.cpp
@@ -15,6 +15,7 @@
 
 #include "game_state.h"
 #include "mesh.h"
+#include "shader.h"
 
 #define M_PI 3.14159265358979323846
 
@@ -55,9 +56,6 @@ void RenderBitmapString(float x, float y, void* font, const char* string)
 
 // 필요 변수 선언
 GLint width, height;
-GLchar* vertexSource, * fragmentSource;
-GLuint vertexShader, fragmentShader;
-GLuint shaderProgramID;
 
 // 필수 함수 정의
 GLvoid drawScene(GLvoid);
@@ -72,12 +70,6 @@ bool is_mouse_on_camera = false;
 bool is_mouse_on_handle = false;
 void MouseMotion(int x, int y);
 void MouseButton(int button, int state, int x, int y);
-
-// 세이더 정의
-char* filetobuf(const char* file);
-void make_vertexShaders();
-void make_fragmentShaders();
-void make_shaderProgram();
 
 // 조명***
 bool isLight = true;	// 조명
@@ -833,8 +825,7 @@ int main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 
 	Mesh_InitAll();
 
-	//--- 세이더 읽어와서 세이더 프로그램 만들기
-	make_shaderProgram();
+	Shader_InitAll();
 
 	unsigned int lightPosLocation = glGetUniformLocation(shaderProgramID, "lightPos");
 	glUniform3f(lightPosLocation, lightX, lightY, lightZ);
@@ -1730,92 +1721,6 @@ void MouseMotion(int x, int y)
 	// 화면 갱신 요청
 	glutPostRedisplay();
 }
-
-// 세이더
-char* filetobuf(const char* file)
-{
-	FILE* fptr;
-	long length;
-	char* buf;
-	fptr = fopen(file, "rb");			// Open file for reading 
-	if (!fptr)							// Return NULL on failure 
-		return NULL;
-	fseek(fptr, 0, SEEK_END);			// Seek to the end of the file 
-	length = ftell(fptr);				// Find out how many bytes into the file we are 
-	buf = (char*)malloc(length + 1);	// Allocate a buffer for the entire length of the file and a null terminator 
-	fseek(fptr, 0, SEEK_SET);			// Go back to the beginning of the file 
-	fread(buf, length, 1, fptr);		// Read the contents of the file in to the buffer 
-	fclose(fptr);						// Close the file 
-	buf[length] = 0;					// Null terminator 
-	return buf;							// Return the buffer 
-}
-void make_vertexShaders()
-{
-	vertexSource = filetobuf("vertex.glsl");
-
-	//--- 버텍스 세이더 객체 만들기
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
-
-	//--- 버텍스 세이더 컴파일하기
-	glCompileShader(vertexShader);
-
-	//--- 컴파일이 제대로 되지 않은 경우: 에러 체크
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
-		return;
-	}
-}
-void make_fragmentShaders()
-{
-	fragmentSource = filetobuf("fragment.glsl");
-
-	//--- 프래그먼트 세이더 객체 만들기
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	//--- 세이더 코드를 세이더 객체에 넣기
-	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
-
-	//--- 프래그먼트 세이더 컴파일
-	glCompileShader(fragmentShader);
-
-	//--- 컴파일이 제대로 되지 않은 경우: 컴파일 에러 체크
-	GLint result;
-	GLchar errorLog[512];
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
-	if (!result)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
-		std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
-		return;
-	}
-}
-void make_shaderProgram()
-{
-	make_vertexShaders(); //--- 버텍스 세이더 만들기
-	make_fragmentShaders(); //--- 프래그먼트 세이더 만들기
-
-	//-- shader Program
-	shaderProgramID = glCreateProgram();
-	glAttachShader(shaderProgramID, vertexShader);
-	glAttachShader(shaderProgramID, fragmentShader);
-	glLinkProgram(shaderProgramID);
-
-	//--- 세이더 삭제하기
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	//--- Shader Program 사용하기
-	glUseProgram(shaderProgramID);
-}
-
 
 
 
