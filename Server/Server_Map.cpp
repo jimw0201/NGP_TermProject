1029b7cdb1bf50b0aa65371cdd61ce59a1d5c13f
2021180033-LTH <mdk73000@naver.com>
2025-12-10 23:47:14 +0900

    Server Change
    Server Change

--- Server/Server_Map.cpp
+++ Server/Server_Map.cpp
@@ -387,13 +387,13 @@ void Server_LoadMap(int stage)
         // 거대한 벽 2개
         g_obstacles[0][0].x = 0.0f;
         g_obstacles[0][0].z = -4.5f;
-        g_obstacles[0][0].rotYDeg = 0.0f;
+        g_obstacles[0][0].rotYDeg = 90.0f;
         g_obstacles[0][0].scaleX = 2.0f;
         g_obstacles[0][0].scaleZ = 6.0f;
 
         g_obstacles[0][1].x = 0.0f;
         g_obstacles[0][1].z = 4.5f;
-        g_obstacles[0][1].rotYDeg = 0.0f;
+        g_obstacles[0][1].rotYDeg = 90.0f;
         g_obstacles[0][1].scaleX = 2.0f;
         g_obstacles[0][1].scaleZ = 6.0f;
     }
